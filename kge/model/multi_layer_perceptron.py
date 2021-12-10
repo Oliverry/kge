47b836aa0579e710344ce9fd60c8e6d5cc8adcb1
Oliverry <oliver.v.h@tutanota.com>
2021-12-10 10:37:38 +0100

    added basic multilayer perceptron model

---
+++ kge/model/multi_layer_perceptron.py
@@ -0,0 +1,44 @@
+from torch import nn
+
+from kge import Config, Dataset
+from kge.job import Job
+from kge.model.kge_model import RelationalScorer, KgeModel
+
+
+class MultiLayerPerceptronScorer(RelationalScorer):
+
+    def __init__(self, config: Config, dataset: Dataset, configuration_key=None):
+        super().__init__(config, dataset, configuration_key)
+        self._norm = self.get_option("l_norm")
+        self.linear1 = nn.Linear(128, 100)
+        self.linear2 = nn.Linear(100, 1)
+
+
+class MultiLayerPerceptron(KgeModel):
+
+    def __init__(
+            self,
+            config: Config,
+            dataset: Dataset,
+            configuration_key=None,
+            init_for_load_only=False,
+    ):
+        super().__init__(
+            config=config,
+            dataset=dataset,
+            scorer=MultiLayerPerceptronScorer,
+            configuration_key=configuration_key,
+            init_for_load_only=init_for_load_only,
+        )
+
+    def prepare_job(self, job: Job, **kwargs):
+        super().prepare_job(job, **kwargs)
+
+        from kge.job import TrainingJobNegativeSampling
+
+        if (
+                isinstance(job, TrainingJobNegativeSampling)
+                and job.config.get("negative_sampling.implementation") == "auto"
+        ):
+            # TransE with batch currently tends to run out of memory, so we use triple.
+            job.config.set("negative_sampling.implementation", "triple", log=True)
