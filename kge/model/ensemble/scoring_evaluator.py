6e994b3663cf2d949c56d24405acdb21ec098e11
Oliverry <oliver.v.h@tutanota.com>
2022-01-10 12:56:55 +0100

    removed unused saving and loading

--- kge/model/ensemble/scoring_evaluator.py
+++ kge/model/ensemble/scoring_evaluator.py
@@ -20,12 +20,6 @@ class ScoringEvaluator(torch.nn.Module, Configurable):
         """
         raise NotImplementedError
 
-    def save(self):
-        raise NotImplementedError
-
-    def load(self, savepoint):
-        raise NotImplementedError
-
 
 class AvgScoringEvaluator(ScoringEvaluator):
 
@@ -35,12 +29,6 @@ class AvgScoringEvaluator(ScoringEvaluator):
     def forward(self, scores: Tensor) -> Tensor:
         res = torch.mean(scores, dim=1)
         return res
-
-    def save(self):
-        pass
-
-    def load(self, savepoint):
-        pass
 
 
 class PlattScalingEvaluator(ScoringEvaluator):
@@ -65,16 +53,6 @@ class PlattScalingEvaluator(ScoringEvaluator):
         res = torch.mean(res, dim=1)
         return res
 
-    def save(self):
-        state = {}
-        for idx, scaler in enumerate(self.scalers):
-            state[idx] = scaler.save()
-        return state
-
-    def load(self, savepoint):
-        for idx, scaler in enumerate(self.scalers):
-            scaler.load(savepoint[idx])
-
 
 class PlattScaler(nn.Module):
 
@@ -87,10 +65,4 @@ class PlattScaler(nn.Module):
         t = self.linear(scores)
         t = self.sigmoid(-t)
         return t
-
-    def save(self):
-        return self.state_dict()
-
-    def load(self, savepoint):
-        self.load_state_dict(savepoint)
 
