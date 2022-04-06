5eeda6713527ca1a6f43397f37479dcb43a9e2d6
Oliverry <oliver.v.h@tutanota.com>
2022-04-06 16:25:36 +0200

    added efficient implementation of mlp and sme

--- kge/model/multilayer_perceptron.py
+++ kge/model/multilayer_perceptron.py
@@ -16,9 +16,9 @@ class MultiLayerPerceptronScorer(RelationalScorer):
         layer_dim = self.get_option("layer_dim")
         if layer_dim < 0:
             layer_dim = entity_dim
-        self.linear1 = nn.Linear(dim_in, layer_dim)  # embedding concatenation (spo) -> entitiy embedding size
+        self.linear1 = nn.Linear(dim_in, layer_dim)
         self.tanh = nn.Tanh()
-        self.linear2 = nn.Linear(layer_dim, 1)  # entity embedding size -> 1
+        self.linear2 = nn.Linear(layer_dim, 1)
         self.sigmoid = nn.Sigmoid()
 
     def score_emb_spo(self, s_emb: Tensor, p_emb: Tensor, o_emb: Tensor) -> Tensor:
@@ -28,6 +28,40 @@ class MultiLayerPerceptronScorer(RelationalScorer):
         embeds = self.linear2(embeds)
         scores = self.sigmoid(embeds)
         return scores
+
+    def score_emb(self, s_emb: Tensor, p_emb: Tensor, o_emb: Tensor, combine: str) -> Tensor:
+        n = p_emb.size(0)
+        if combine == "spo":
+            embeds = torch.cat((s_emb, p_emb, o_emb), 1)
+            embeds = self.linear1(embeds)
+            embeds = self.tanh(embeds)
+            embeds = self.linear2(embeds)
+            out = self.sigmoid(embeds)
+        elif combine == "sp_":
+            score_list = []
+            for o_emb_single in o_emb:
+                o_emb_rep = o_emb_single.repeat((n, 1))
+                embeds = torch.cat((s_emb, p_emb, o_emb_rep), 1)
+                embeds = self.linear1(embeds)
+                embeds = self.tanh(embeds)
+                embeds = self.linear2(embeds)
+                embeds_score = self.sigmoid(embeds)
+                score_list.append(embeds_score)
+            out = torch.cat(score_list, 1)
+        elif combine == "_po":
+            score_list = []
+            for s_emb_single in s_emb:
+                s_emb_rep = s_emb_single.repeat((n, 1))
+                embeds = torch.cat((s_emb_rep, p_emb, o_emb), 1)
+                embeds = self.linear1(embeds)
+                embeds = self.tanh(embeds)
+                embeds = self.linear2(embeds)
+                embeds_score = self.sigmoid(embeds)
+                score_list.append(embeds_score)
+            out = torch.cat(score_list, 1)
+        else:
+            return super().score_emb(s_emb, p_emb, o_emb, combine)
+        return out.view(n, -1)
 
 
 class MultilayerPerceptron(KgeModel):
--- kge/model/semantic_matching_energy.py
+++ kge/model/semantic_matching_energy.py
@@ -18,27 +18,64 @@ class SmeScorer(RelationalScorer):
             layer_dim = entity_dim
         if self.g_func == "linear":
             self.sp_linear = torch.nn.Linear(entity_dim + relation_dim, layer_dim, bias=True)
-            self.op_linear = torch.nn.Linear(relation_dim + entity_dim, layer_dim, bias=True)
+            self.po_linear = torch.nn.Linear(relation_dim + entity_dim, layer_dim, bias=True)
         elif self.g_func == "bilinear":
             self.sp_bilinear = torch.nn.Bilinear(entity_dim, relation_dim, entity_dim, bias=True)
             self.po_bilinear = torch.nn.Bilinear(relation_dim, entity_dim, entity_dim, bias=True)
         else:
             raise ValueError
 
-    def score_emb_spo(self, s_emb: Tensor, p_emb: Tensor, o_emb: Tensor) -> Tensor:
-        n = s_emb.size()[0]
-        if self.g_func == "linear":
-            sp_embed = torch.cat((s_emb, p_emb), 1)
-            op_embed = torch.cat((o_emb, p_emb), 1)
-            sp_embed = self.sp_linear(sp_embed)
-            op_embed = self.op_linear(op_embed)
-        elif self.g_func == "bilinear":
-            sp_embed = self.sp_bilinear(s_emb, p_emb)
-            op_embed = self.sp_bilinear(p_emb, o_emb)
+    def score_emb(self, s_emb: Tensor, p_emb: Tensor, o_emb: Tensor, combine: str) -> Tensor:
+        n = p_emb.size(0)
+        if combine == "spo":
+            if self.g_func == "linear":
+                sp_embed = torch.cat((s_emb, p_emb), 1)
+                po_embed = torch.cat((p_emb, o_emb), 1)
+                sp_embed = self.sp_linear(sp_embed)
+                po_embed = self.po_linear(po_embed)
+            elif self.g_func == "bilinear":
+                sp_embed = self.sp_bilinear(s_emb, p_emb)
+                po_embed = self.sp_bilinear(p_emb, o_emb)
+            else:
+                raise ValueError
+            out = torch.einsum('ij,ij->i', sp_embed, po_embed)  # compute row-wise dot product
+        elif combine == "sp_":
+            score_list = []
+            for o_emb_single in o_emb:
+                o_emb_rep = o_emb_single.repeat((n, 1))
+                if self.g_func == "linear":
+                    sp_embed = torch.cat((s_emb, p_emb), 1)
+                    po_embed = torch.cat((p_emb, o_emb_rep), 1)
+                    sp_embed = self.sp_linear(sp_embed)
+                    po_embed = self.po_linear(po_embed)
+                elif self.g_func == "bilinear":
+                    sp_embed = self.sp_bilinear(s_emb, p_emb)
+                    po_embed = self.sp_bilinear(p_emb, o_emb_rep)
+                else:
+                    raise ValueError
+                embed_score = torch.einsum('ij,ij->i', sp_embed, po_embed)
+                score_list.append(embed_score)
+            out = torch.stack(score_list, 1)
+        elif combine == "_po":
+            score_list = []
+            for s_emb_single in s_emb:
+                s_emb_rep = s_emb_single.repeat((n, 1))
+                if self.g_func == "linear":
+                    sp_embed = torch.cat((s_emb_rep, p_emb), 1)
+                    po_embed = torch.cat((p_emb, o_emb), 1)
+                    sp_embed = self.sp_linear(sp_embed)
+                    po_embed = self.po_linear(po_embed)
+                elif self.g_func == "bilinear":
+                    sp_embed = self.sp_bilinear(s_emb_rep, p_emb)
+                    po_embed = self.sp_bilinear(p_emb, o_emb)
+                else:
+                    raise ValueError
+                embed_score = torch.einsum('ij,ij->i', sp_embed, po_embed)
+                score_list.append(embed_score)
+            out = torch.stack(score_list, 1)
         else:
-            raise ValueError
-        res = torch.einsum('ij,ij->i', sp_embed, op_embed)  # compute row-wise dot product
-        return res.view(n)
+            return super().score_emb(s_emb, p_emb, o_emb, combine)
+        return out.view(n, -1)
 
 
 class SemanticMatchingEnergy(KgeModel):
